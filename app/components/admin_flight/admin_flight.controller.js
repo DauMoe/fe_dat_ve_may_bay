33715aa2f26065df7e96d3af4ff91af140aae376
MoeDay <lehuyhoang1111999@gmail.com>
2022-05-31 13:08:09 +0700

    reset search keyword after query ok

--- app/components/admin_flight/admin_flight.controller.js
+++ app/components/admin_flight/admin_flight.controller.js
@@ -218,10 +218,21 @@ angular
                 .then(r => {
                     $scope.ListFlightSchedule = [];
                     if (r.data.code === 200) {
+                        $scope.SearchInfo   = {
+                            from_airport    : "",
+                            to_airport      : "",
+                            start_date      : "",
+                            end_date        : ""
+                        };
                         $scope.ListFlightSchedule   = r.data.result.list;
                         $scope.FlightSchedule       = null;
                     } else if(r.data.code === 400) {
-                        
+                        $scope.SearchInfo   = {
+                            from_airport    : "",
+                            to_airport      : "",
+                            start_date      : "",
+                            end_date        : ""
+                        };
                     } else {
                         CatchEx(r.data);
                     }
